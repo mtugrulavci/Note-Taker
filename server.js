6d42b51e3f525555c5660f665685f519174db106
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-07-01 00:46:39 -0400

    static public folder created

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 // Dependencies
 // =============================================================
 const fs = require('fs');// added because we will write 
+
 const express = require('express');
-const path = require('path');
-const { notes } = require('./Develop/db/db');
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const path = require('path');// it will be used for sendFiles
+const { notes } = require('./db/db.json');
+
+//set a static folder - i could not be able to use css file without this 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/*app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
-  }); */
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
 // Create an `/notes` route that returns `add.html`
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, 'public','notes.html'));
+  });
+  app.get('/api/notes', (req, res) => {
+    return res.json(notes);
   });
 
   
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
