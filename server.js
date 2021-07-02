8a3bbfbb47138929246df568fd925b61d2ce2890
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-07-02 03:22:11 -0400

    wildcard location brought to bottom

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
+
 // Create an `/notes` route that returns `add.html`
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','notes.html'));
@@ -59,6 +60,9 @@ app.get('/api/notes', (req, res) => {
       res.json(notes);
  });
 
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+}); 
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
