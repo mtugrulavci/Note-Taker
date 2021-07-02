77b9380dddbe02fd02042ca08208265a8824f10e
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-07-02 02:41:47 -0400

    bg fix and delete function added

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const path = require('path');// it will be used for sendFiles
-const { notes } = require('./db/db.json');
-const { runInNewContext } = require('vm');
 
+
+
+const { Console } = require('console');
 //set a static folder - i could not be able to use css file without this 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 
 app.get('/', (req, res) => {
@@ -30,30 +32,38 @@ app.get('/notes', (req, res) => {
 
   // Gets all notes
 app.get('/api/notes', (req, res) => {
-    return res.json(notes);
+   // let { notes } = require('./db/db.json');
+    let notes= JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  res.json(notes);
   });
 
   //Create a note
-app.post('/api/notes', (req, res)=>{
+  app.post('/api/notes', (req, res)=>{
     const newNote = {
         title: req.body.title,
         text: req.body.text,
         id: uuid.v4()
     }
+   // let {notes}  = require('./db/db.json');
+   let notes= JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
     notes.push(newNote); // pushes the new entry to the notes array
     //writes the new entry to the jason file
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes }, null, 2)
-      );
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+      res.json(notes);
   });
-/*
-  app.delete('api/notes:id', (req,res)=>{
-   const a = notes.filter(notes => notes.id === id)[0];
-   
-   notes.push(a);
-  })
-  */
+
+  app.delete(`/api/notes/:id`, (req,res)=>{
+    let notes= JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+   //let { notes } = require('./db/db.json');
+    notes = notes.filter(notes=> notes.id.toString()!== req.params.id.toString());
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
+      res.json(notes);
+ });
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
